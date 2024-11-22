859d8a0e9e22e35d864df632ca95ef3c5fa7b3bd
Erubey10 <mrm75499@lasallebajio.edu.mx>
2024-11-22 17:53:09 -0600

    ya finalizar

---
+++ lib/profile_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  final String name;
+  final String age;
+  final String occupation;
+
+  ProfileScreen({required this.name, required this.age, required this.occupation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Perfil')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Nombre: $name', style: TextStyle(fontSize: 18)),
+            Text('Edad: $age', style: TextStyle(fontSize: 18)),
+            Text('Ocupación: $occupation', style: TextStyle(fontSize: 18)),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.popUntil(context, (route) => route.isFirst);
+              },
+              child: Text('Regresar a Inicio'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
